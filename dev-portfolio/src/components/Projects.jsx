e9dd7ffa4c8dca1f999660bb1d005f72a2a04ed6
thecoachanna <nettles.anna@gmail.com>
2022-09-20 11:17:55 -0400

    starting-strength deploy added

--- dev-portfolio/src/components/Projects.jsx
+++ dev-portfolio/src/components/Projects.jsx
@@ -75,7 +75,7 @@ const Projects = () => {
                 MERN App
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a href="https://starting-strength-frontend.herokuapp.com/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
